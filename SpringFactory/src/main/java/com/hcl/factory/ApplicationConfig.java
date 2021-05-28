091a969335aa9ebc44365d486d153c19c4266827
Tom Aronson <thomas.aronson@hcl.com>
2021-05-28 11:37:58 -0400

    Spring factory pattern

---
+++ SpringFactory/src/main/java/com/hcl/factory/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.hcl.factory;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = ApplicationConfig.class)
+public class ApplicationConfig {
+}
