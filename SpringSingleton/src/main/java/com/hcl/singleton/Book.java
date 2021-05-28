3f774aa6bf0e81ffcc8b78cf3655d18b71a2d59e
Tom Aronson <thomas.aronson@hcl.com>
2021-05-28 11:41:05 -0400

    Spring singleton pattern

---
+++ SpringSingleton/src/main/java/com/hcl/singleton/Book.java
@@ -0,0 +1,16 @@
+package com.hcl.singleton;
+
+public class Book {
+	
+	private Long id;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+}
---
+++ SpringSingleton/src/main/java/com/hcl/singleton/BookController.java
@@ -0,0 +1,21 @@
+package com.hcl.singleton;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookRepository repository;
+
+	@GetMapping("/book/{id}")
+	public Book findById(@PathVariable long id) {
+		System.out.println(repository);
+		return repository.findById(id).get();
+	}
+
+}
